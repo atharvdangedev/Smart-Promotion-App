942c00298529626d6eeafe15940db4d4745a4e7c
Sangram-Deshmukh <sangram.d@smartscripts.in>
2025-06-20 16:27:01 +0530

    initial Setup Complete

---
+++ Mobile/App.js
@@ -0,0 +1,12 @@
+import "./global.css"
+import { Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-gray-500">
+      <Text className="text-xl font-bold text-blue-500">
+        Welcome to Nativewind!
+      </Text>
+    </View>
+  );
+}
