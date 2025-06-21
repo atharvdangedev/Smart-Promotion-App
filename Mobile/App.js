4d3c9be56882e364c04b9f3c11041b2486e191df
Sangram-Deshmukh <sangram.d@smartscripts.in>
2025-06-21 12:29:15 +0530

    Implemented navigation setup

--- Mobile/App.js
+++ Mobile/App.js
@@ -1,12 +1,25 @@
-import "./global.css"
-import { Text, View } from "react-native";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import WelcomeScreen from './screens/WelcomeScreen';
+import HomeScreen from './screens/HomeScreen';
+import SignInScreen from './screens/SignInScreen';
+import EnterpriseLoginScreen from './screens/EnterpriseLoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-gray-500">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name='Home' component={HomeScreen} />
+        <Stack.Screen name='SignIn' component={SignInScreen} />
+        <Stack.Screen name='EnterpriseLogin' component={EnterpriseLoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
---
+++ Mobile/screens/SignInScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function SignInScreen() {
+    return (
+        <View className="flex-1 justify-center items-center bg-white">
+            <Text className="text-black text-xl">Sign In Screen</Text>
+        </View>
+    );
+}
