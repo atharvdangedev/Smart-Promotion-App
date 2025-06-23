44168d9a34a70518ca5a7d199489db7cd63a923e
Sangram-Deshmukh <sangram.d@smartscripts.in>
2025-06-23 11:05:26 +0530

    Added Forgot and Reset Password screens

---
+++ Mobile/screens/ForgotPassword.js
@@ -0,0 +1,18 @@
+import { View, Text, SafeAreaView, TouchableOpacity } from 'react-native'
+import React from 'react'
+import InputField from '../components/InputField'
+
+export default function ForgotPassword({ navigation }) {
+    return (
+        <SafeAreaView className='flex-1 justify-center px-6 bg-white'>
+            <Text className="text-2xl font-semibold mb-8 text-black"> Enter Your Email here,</Text>
+            <InputField icon='user' placeholder="Email" />
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate('ResetPassword')}
+                className="bg-black py-3 rounded-xl mb-4 mt-4">
+                <Text className="text-center text-white font-semibold">Get Token</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    )
+}
--- Mobile/screens/SignInScreen.js
+++ Mobile/screens/SignInScreen.js
@@ -1,10 +1,38 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity, SafeAreaView } from 'react-native';
+import InputField from '../components/InputField';
 
-export default function SignInScreen() {
+export default function LoginScreen({ navigation }) {
     return (
-        <View className="flex-1 justify-center items-center bg-white">
-            <Text className="text-black text-xl">Sign In Screen</Text>
-        </View>
+        <SafeAreaView className="flex-1 justify-center px-6 bg-white">
+            <Text className="text-2xl font-semibold mb-20 text-black">Welcome Back Glad to see you, Again!</Text>
+
+            <InputField icon="user" placeholder="Email" />
+            <InputField icon="lock" placeholder="Password" secureTextEntry />
+
+            <TouchableOpacity
+                onPress={() => navigation.navigate('ForgotPassword')}
+                className="mb-4">
+                <Text className="text-right text-sm text-gray-500">Forgot password?</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity className="bg-black py-3 rounded-xl mb-4">
+                <Text className="text-center text-white font-semibold">Login</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity onPress={() => navigation.navigate('ForgotPassword')}>
+                <Text className="text-center text-gray-500 mb-3">Reset Password</Text>
+            </TouchableOpacity>
+
+            {/* <View className="flex-row justify-center space-x-6 mb-6">
+                <Text>🌐</Text><Text>📘</Text><Text>🍎</Text>
+            </View> */}
+
+            <TouchableOpacity onPress={() => navigation.navigate('EnterpriseLogin')}>
+                <Text className="text-center text-sm text-gray-500">
+                    Don’t have an account? <Text className="font-semibold text-black">Sign Up</Text>
+                </Text>
+            </TouchableOpacity>
+        </SafeAreaView>
     );
 }
